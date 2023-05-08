6de4e0b448c42a7d7c807422d7269517986f2e6a
AlexandreFelippe <alemfelippe@gmail.com>
2023-05-08 18:49:09 -0300

    Resolve exercícios Parte 01

---
+++ fundamentos/fundamentos-secao03-aula05/Exercicio-parte01/exercicio03.js
@@ -0,0 +1,5 @@
+//🚀 Modifique a variável para que não ocorra Erro.
+//Copie o código a seguir e rode-o para verificar sua saída.
+let favoriteFood = 'Lasanha';
+favoriteFood = 'Hambúrguer';
+console.log(favoriteFood);
---
+++ fundamentos/fundamentos-secao03-aula05/treino.js
@@ -0,0 +1,9 @@
+const nome = "Gus";
+const modulos = [1];
+const empresa = {
+  nome: "Trybe",
+  nota: 8001,
+};
+modulos.push(3);
+nome = "Batman";
+empresa.nota = 8002;
