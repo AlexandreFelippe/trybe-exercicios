7e4e37192c5e313d654edd42c9402271fbd476d8
AlexandreFelippe <alemfelippe@gmail.com>
2023-05-11 18:35:57 -0300

    Inclui arquivos

---
+++ fundamentos/fundamentos-secao04-aula01/script.js
@@ -0,0 +1,7 @@
+//Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome de seu filme favorito.
+//Depois, recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+//Por fim, recupere o subtítulo e altere-o também.
+
+document.getElementById("page-title").innerText="TROCA TÍTULO";
+document.getElementById("second-paragraph").innerText="Consegui trocar o parágrafo também";
+document.getElementById("subtitle").innerText="Começando bem";
