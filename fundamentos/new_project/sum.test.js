9c21b8e2a34bb93c8a23e89935e5dbf2090aac61
AlexandreFelippe <alemfelippe@gmail.com>
2023-05-31 15:54:59 -0300

    resolve mais exercicio

--- fundamentos/new_project/sum.test.js
+++ fundamentos/new_project/sum.test.js
@@ -1,5 +1,10 @@
-const sum = (a, b) => a + b;
-
-test('sums two values', () => {
-  expect(sum(2, 3)).toEqual(5);
-});
+const removeItem = require('./sun.js');
+  
+  describe('testa a função removeItem', () => {
+      it('retorna um array sem o item a ser retirado', () => {
+      expect(removeItem([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+      expect(removeItem([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+      expect(removeItem([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+    });
+  });
+  
